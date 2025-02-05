7d7c79c2d244c64f2b8dbf4ecb87c811627b2922
Suvrat Ketkar <suvratketkar@gmail.com>
2025-02-05 16:51:38 +0530

    Updated doctor Model

--- src/models/doctorModel.js
+++ src/models/doctorModel.js
@@ -1,22 +1,118 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-const doctorSchema = new mongoose.Schema({
-    name: {type:String, required:true},
-    email: {type:String, required:true, unique:true},
-    password: {type:String, required:true},
-    image: {type:String,required:true},
-    specialization: {type:String, required:true},
-    degree: {type:String, required:true},
-    experience: {type:String, required:true},
-    about: {type:String, required:true},
-    availability: {type:Boolean, required:true},
-    fees: {type:Number, required:true},
-    address: {type:Object, required:true},
-    date: {type:Number, required:true},
-    slots_booked: {type:Object,default:{}}
+// Doctor Schema
+const doctorSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+      default:
+            "https://www.shutterstock.com/image-vector/default-placeholder-doctor-halflength-portrait-260nw-1058724875.jpg"
+    },
+    specialization: {
+      type: String,
+      required: true,
+    },
+    degree: {
+      type: String,
+      required: true,
+    },
+    experience: {
+      type: String,
+      required: true,
+    },
+    about: {
+      type: String,
+      required: true,
+    },
+    availability: {
+      type: Boolean,
+      required: true,
+    },
+    fees: {
+      type: Number,
+      required: true,
+    },
+    address: {
+      type: Object,
+      required: true,
+    },
+    date: {
+      type: Number,
+      required: true,
+    },
+    slots_booked: {
+      type: Object,
+      default: {},
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-},{minimize:false})
+// Pre-save hook to hash the password before saving
+doctorSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
-const doctorModel = mongoose.models.doctor || mongoose.model('doctor',doctorSchema);
+// Method to compare password
+doctorSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
-export default doctorModel
+// Method to generate access token
+doctorSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      name: this.name,
+      specialization: this.specialization,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+// Method to generate refresh token
+doctorSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+// Create and export doctor model
+const Doctor = mongoose.models.doctor || mongoose.model("doctor", doctorSchema);
+
+export default Doctor;
